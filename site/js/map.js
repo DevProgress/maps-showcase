42afc693d011519a35e70a3769f2a25a74804483
Eli Luberoff <eli@desmos.com>
2016-08-10 23:01:22 -0700

    hook up initial map
    based on and using: https://bl.ocks.org/mbostock/2206590

---
+++ site/js/map.js
@@ -0,0 +1,74 @@
+var initMap = function () {
+
+  var width = 960,
+      height = 500,
+      centered;
+
+  var projection = d3.geo.albersUsa()
+      .scale(1070)
+      .translate([width / 2, height / 2]);
+
+  var path = d3.geo.path()
+      .projection(projection);
+
+  var svg, g;
+
+  d3.json("/js/us.json", function(error, us) {
+
+    document.getElementById('map-container').innerHTML = '';
+
+    svg = d3.select("#map-container").append("svg")
+        .attr("width", width)
+        .attr("height", height);
+
+    svg.append("rect")
+        .attr("class", "background")
+        .attr("width", width)
+        .attr("height", height)
+        .on("click", clicked);
+
+    g = svg.append("g");
+
+    if (error) throw error;
+
+    g.append("g")
+        .attr("id", "states")
+      .selectAll("path")
+        .data(topojson.feature(us, us.objects.states).features)
+      .enter().append("path")
+        .attr("d", path)
+        .on("click", clicked);
+
+    g.append("path")
+        .datum(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; }))
+        .attr("id", "state-borders")
+        .attr("d", path);
+  });
+
+  function clicked(d) {
+    var x, y, k;
+
+    if (d && centered !== d) {
+      var centroid = path.centroid(d);
+      x = centroid[0];
+      y = centroid[1];
+      k = 4;
+      centered = d;
+    } else {
+      x = width / 2;
+      y = height / 2;
+      k = 1;
+      centered = null;
+    }
+
+    g.selectAll("path")
+        .classed("active", centered && function(d) { return d === centered; });
+
+    g.transition()
+        .duration(750)
+        .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")scale(" + k + ")translate(" + -x + "," + -y + ")")
+        .style("stroke-width", 1.5 / k + "px");
+  }
+};
+
+initMap();
