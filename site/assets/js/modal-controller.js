bd6815f49b4fac807664470481f267cad5c07794
Eli Luberoff <eli@desmos.com>
2016-08-12 01:57:36 -0700

    hook up next / previous

--- site/assets/js/modal-controller.js
+++ site/assets/js/modal-controller.js
@@ -2,8 +2,14 @@ var modalController = (function () {
   var states = {
     MA: {
       name: 'Massachusetts'
+    },
+    CA: {
+      name: 'California'
     }
   };
+
+  var stateList = ['CA', 'MA'];
+  var selectedStateIdx = null;
 
   var modal = document.getElementById('mainModal');
   var background = document.getElementById('modalBackdrop');
@@ -11,27 +17,46 @@ var modalController = (function () {
   var hideModal = function () {
     modal.className = 'modal fade';
     background.className = 'modal-backdrop fade';
+    selectedStateIdx = null;
 
     setTimeout(function () {
       modal.style.display = 'none';
     }, 250); //long enough for animation to wrap up
   };
 
-  var showState = function (state) {
+  var populateStateData = function (state) {
+    selectedState = state;
     var stateData = states[state];
+    selectedStateIdx = stateList.indexOf(state);
+
+    //TODO: populate the rest of this modal
+    document.getElementById('state-title').innerText = stateData.name;
+  }
+
+  var showState = function (state) {
     modal.style.display = 'block';
     setTimeout(function () {
       modal.className = 'modal fade in';
       background.className = 'modal-backdrop fade in';
     }, 50); //long enough to be at least one animation frame, so that the animation triggers
-
-    //TODO: populate the rest of this modal
-    document.getElementById('state-title').innerText = stateData.name;
+    populateStateData(state);
   };
+
+  var nextState = function () {
+    if (selectedStateIdx >= stateList.length - 1) return;
+    populateStateData(stateList[selectedStateIdx + 1]);
+  }
+
+  var previousState = function () {
+    if (selectedStateIdx == 0) return;
+    populateStateData(stateList[selectedStateIdx - 1]);
+  }
 
   return {
     showState: showState,
-    hideModal: hideModal
+    hideModal: hideModal,
+    nextState: nextState,
+    previousState: previousState
   }
 
 })();
