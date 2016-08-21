a078ea17c317fb46cc9aed636a7b0a47d2609888
kahneraja@gmail.com <kahneraja@gmail.com>
2016-08-22 03:24:44 +1000

    poc on pinch to zoom with pattern img for a given state.

--- site/assets/js/map.js
+++ site/assets/js/map.js
@@ -1,99 +1,33 @@
-var g;
+var stage = document.getElementById('stage');
+$stage = jQuery(stage);
+var manager = new Hammer.Manager(stage);
+var Pan = new Hammer.Pan();
+var Pinch = new Hammer.Pinch();
 
-var initMap = function () {
-
-  var width = document.documentElement.clientWidth || document.body.clientWidth,
-      height = width * 500 / 960,
-      centered;
-
-  var projection = d3.geo.albersUsa()
-      .scale(width)
-      .translate([width / 2, height / 2]);
-
-  var path = d3.geo.path()
-      .projection(projection);
-
-  d3.json("assets/js/us.json", function(error, us) {
-    var mapContainer = document.getElementById('map-container');
-    mapContainer.innerHTML = '';
-    mapContainer.style.height = height * 1.05;
-
-    svg = d3.select("#map-container").append("svg")
-        .attr("width", width)
-        .attr("height", height);
-
-    svg.append("rect")
-        .attr("class", "background")
-        .attr("width", width)
-        .attr("height", height)
-        .on("click", clicked);
-
-    g = svg.append("g");
-    if (error) throw error;
-
-    g.append("g")
-        .attr("id", "states")
-      .selectAll("path")
-        .data(topojson.feature(us, us.objects.states).features)
-      .enter().append("path")
-        .attr("d", path)
-        .on("click", clicked);
-
-    g.append("path")
-        .datum(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; }))
-        .attr("id", "state-borders")
-        .attr("d", path);
-  });
-
-
-  function clicked(d) {
-    //looks like we can tell the state from d.id.
-    var state = states_data[d.id];
-    state.id = d.id;
-
-    modalController.showState(state);
-
-    var x, y, k;
-
-    var centroid = path.centroid(d);
-    x = centroid[0];
-    y = centroid[1];
-    k = 4;
-    centered = d;
-
-    g.selectAll("path").classed("active", centered && function(d) { return d === centered; });
-
-    /*
-    Don't zoom. Do select the state.
-
-    var x, y, k;
-
-    if (d && centered !== d) {
-      var centroid = path.centroid(d);
-      x = centroid[0];
-      y = centroid[1];
-      k = 4;
-      centered = d;
-    } else {
-      x = width / 2;
-      y = height / 2;
-      k = 1;
-      centered = null;
-    }
-
-    g.selectAll("path")
-        .classed("active", centered && function(d) { return d === centered; });
-
-    g.transition()
-        .duration(750)
-        .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")scale(" + k + ")translate(" + -x + "," + -y + ")")
-        .style("stroke-width", 1.5 / k + "px");
-    */
-  }
-};
-
-initMap();
-window.addEventListener("resize", function(){
-    initMap();
+manager.add(Pan);
+manager.add(Pinch);
+var liveScale = 0.1;
+var deltaX = 0;
+var deltaY = 0;
+manager.on('panmove', function (e) {
+    var dX = deltaX + e.deltaX;
+    var dY = deltaY + e.deltaY;
+    $.Velocity.hook($stage, 'translateX', dX + 'px');
+    $.Velocity.hook($stage, 'translateY', dY + 'px');
 });
-
+manager.on('panend', function (e) {
+    deltaX = deltaX + e.deltaX;
+    deltaY = deltaY + e.deltaY;
+});
+var currentScale = 1;
+function getRelativeScale(scale) {
+    return scale * currentScale;
+}
+manager.on('pinchmove', function (e) {
+    var scale = getRelativeScale(e.scale);
+    $.Velocity.hook($stage, 'scale', scale);
+});
+manager.on('pinchend', function (e) {
+    currentScale = getRelativeScale(e.scale);
+    liveScale = currentScale;
+});
