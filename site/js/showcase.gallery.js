e184e562d2af8908d9c750d1964339911e5177a9
Andrew Johnson <andrew@gaiagps.com>
2016-08-24 16:38:28 -0700

    kansas is clickable

--- site/js/showcase.gallery.js
+++ site/js/showcase.gallery.js
@@ -1,77 +1,93 @@
-(function(){
+var states_data;
 
-  if (typeof Showcase == 'undefined') { Showcase = {}; }
+d3.json("gen/states-data.json", function(error, info) {
+  states_data = info;
+});
 
-  Showcase.Gallery = {
 
-    $Modal: $([]),
-    Artworks: [],
+var modalController = (function () {
 
-    Init: function(){
-      this.$Modal = $('#myModal');
-      $.ajaxSetup({ cache: false });
-      $.ajax({
-        dataType: "json",
-        url: 'js/artwork.json?v=2',
-        success: function(data){
-          Showcase.Gallery.Artworks = data;
-        },
-        error: function(){
-          console.log(arguments);
-        }
-      });
+  var selectedStateIdx = null;
 
-      // Bind
+  var modal = document.getElementById('mainModal');
+  var background = document.getElementById('modalBackdrop');
 
-      $('.btn-prev', this.$Modal).on('click', function(){
-        Showcase.Gallery.Prev();
-      });
-      $('.btn-next', this.$Modal).on('click', function(){
-        Showcase.Gallery.Next();
-      });
-    },
+  var hideModal = function () {
+    modal.className = 'modal fade';
+    background.className = 'modal-backdrop fade';
+    selectedStateIdx = null;
 
-    Hide: function(id){
-      this.$Modal.modal('hide');
-    },
+    setTimeout(function () {
+      modal.style.display = 'none';
+    }, 250); //long enough for animation to wrap up
 
-    Show: function(id){
-      var artwork = _.find(this.Artworks, { id: id });
-      if (!artwork){
-        artwork = _.first(this.Artworks);
-      }
-
-      $(this.$Modal).attr('data-id', artwork.id);
-      $('.modal-header h4', this.$Modal).text(artwork.title);
-      $('.modal-body p', this.$Modal).text(artwork.description);
-      $('img.artwork', this.$Modal).attr('src', 'img/artwork/' + artwork.id + '.jpg');
-      $('img.pattern', this.$Modal).attr('src', 'img/pattern/' + artwork.id + '.jpg');
-      $('a.btn-danger', this.$Modal).attr('href', 'img/download/' + artwork.id + '.jpg');
-      this.$Modal.modal('show');
-    },
-
-    Next: function(){
-      var id = parseInt($(this.$Modal).attr('data-id'));
-      var index = _.findIndex(this.Artworks, {id:id});
-      var artwork = this.Artworks[index + 1];
-      if (!artwork)
-        artwork = _.first(this.Artworks);
-      this.Show(artwork.id);
-    },
-
-    Prev: function(){
-      var id = parseInt($(this.$Modal).attr('data-id'));
-      var index = _.findIndex(this.Artworks, {id:id});
-      var artwork = this.Artworks[index - 1];
-      if (!artwork)
-        artwork = _.last(this.Artworks);
-      this.Show(artwork.id);
-    }
-
+    g.selectAll("path").classed("active", false);
   };
 
-  $( document ).ready(function(){
-    Showcase.Gallery.Init();
-  });
+  var populateStateData = function (state) {
+    document.getElementById('historical-artifact-image').src = state.historicalArtifactImage;
+    document.getElementById('historical-artifact-image').title = state.historicalArtifactImageTitle;
+    document.getElementById('pattern-image').src = state.patternImage;
+    document.getElementById('pattern-image').title = state.patternImageTitle;
+    document.getElementById('pattern-data').innerText = state.patternData;
+    document.getElementById('state-title').innerText = state.stateName;
+    document.getElementById('historical-artifact-image-title').innerText = state.historicalArtifactImageTitle;
+    document.getElementById('historical-artifact-data').innerText = state.historicalArtifactData;
+  }
 
-}());
+  var showState = function (stateID) {
+    state = states_data[20]; // hard code kansas
+    selectedStateIdx = state.id;
+    modal.style.display = 'block';
+    setTimeout(function () {
+      modal.className = 'modal fade in';
+      background.className = 'modal-backdrop fade in';
+    }, 50); //long enough to be at least one animation frame, so that the animation triggers
+    populateStateData(state);
+  };
+
+  var nextState = function () {
+    g.selectAll("path").classed("active", false);
+
+    selectedStateIdx += 1;
+    // because state ids are not contiguous
+    while(!states_data.hasOwnProperty(selectedStateIdx)){
+      selectedStateIdx += 1;
+      if (selectedStateIdx >= 57){
+        selectedStateIdx = 1;
+      }
+    }
+
+    g.selectAll("path").filter(function (d) { return d.id === selectedStateIdx;}).classed("active", true)
+    var state = states_data[selectedStateIdx];
+    state.id = selectedStateIdx;
+    populateStateData(state);
+  }
+
+  var previousState = function () {
+    g.selectAll("path").classed("active", false);
+
+
+    selectedStateIdx -= 1;
+    // because state ids are not contiguous
+    while(!states_data.hasOwnProperty(selectedStateIdx)){
+      selectedStateIdx -= 1;
+      if (selectedStateIdx <= 0){
+        selectedStateIdx = 56;
+      }
+    }
+
+    g.selectAll("path").filter(function (d) { return d.id === selectedStateIdx;}).classed("active", true)
+    var state = states_data[selectedStateIdx];
+    state.id = selectedStateIdx;
+    populateStateData(state);
+  }
+
+  return {
+    showState: showState,
+    hideModal: hideModal,
+    nextState: nextState,
+    previousState: previousState
+  }
+
+})();
