be22d77c9ad25050f1acb26db81570db1d1ba736
kahneraja <kahneraja@gmail.com>
2016-09-02 14:20:54 -0400

    added hover tooltips.

--- site/js/showcase.bubble.js
+++ site/js/showcase.bubble.js
@@ -12,4 +12,10 @@ $(function() {
   $('g.mini-state-bubble').mouseenter(EnterState);
   $('g.mini-state-bubble').mouseleave(LeaveState);
 
+  $('#map-states g').each(function(){
+    var stateCode = $(this).attr('id');
+    var stateName = states_data[stateCode].stateName;
+    $(this).tooltip({title:stateName, container:'body'});
+  });
+
 });
